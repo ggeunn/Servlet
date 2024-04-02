8d51b77045573049354bc3162c40767f6a6b0a8a
jieun <ggeun1031@naver.com>
2024-04-02 17:26:11 +0900

    [2024-04-02] 수업내용

---
+++ chap05-response-lecture-source/src/main/java/com/ohgiraffers/section03/status/StatusCodeTestServlet.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section03.status;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/status")
+public class StatusCodeTestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//        resp.sendError(404, "페이지가 없다");
+        resp.sendError(500,"서버 내부 오류입니다.");
+
+    }
+}
